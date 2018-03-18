a6604605aa8be3fc2646cf237278633533edeb48
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2018-03-17 17:20:14 -0700

    Specify IMPRESS_GRAPHQL_URL via dotenv
    That way, my silly dev hacks where I keep changing the local Impress IP address can live in `.env.development`, which is now gitignored :P

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React from "react";
 import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { HttpLink } from "apollo-link-http";
+import { IMPRESS_GRAPHQL_URL } from "react-native-dotenv";
+import React from "react";
 
 import createApolloCache from "./src/apollo-cache";
 import Wardrobe from "./src/Wardrobe";
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: "http://192.168.1.249/graphql" }),
+    link: new HttpLink({ uri: IMPRESS_GRAPHQL_URL }),
     cache: createApolloCache(),
 });
 
